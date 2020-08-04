d2e1f24cda8257f49e1ec95178cf170b625a16e2
Cori1109 <chris.jenkins.j1005@gmail.com>
2020-08-04 22:48:32 +1000

    update

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,5 +1,6 @@
 call .\bin\hugo.bat
 git commit -am update
 git subtree split --prefix public -b gh-pages
-git push -f origin gh-pages:master
+REM n.b. removes cutstom domain git push -f origin gh-pages:master
+git push -f origin master
 git branch -D gh-pages
