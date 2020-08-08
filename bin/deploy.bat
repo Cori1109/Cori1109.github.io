84d4d60af971aab31dba1b95150f5a64c69d1ec0
Cori1109 <chris.jenkins.j1005@gmail.com>
2020-08-08 22:31:20 +1000

    update

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,5 +1,5 @@
 call .\bin\hugo.bat
 git commit -am update
 git subtree split --prefix public -b gh-pages
-git push origin gh-pages:master
+git push -f origin gh-pages:master
 git branch -D gh-pages
