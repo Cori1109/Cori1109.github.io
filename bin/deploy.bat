32b80f95c00c9ccf8d5f78bc13681af84a7e166d
Cori1109 <chris.jenkins.j1005@gmail.com>
2021-03-01 20:51:10 +1100

    deploy: /03/- 0-Mo_2051

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -3,7 +3,7 @@ call .\bin\hugo.bat
 set date=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%%time:~3,2%
 
 git commit -am "deploy: %date%"
-git subtree split --prefix public -b gh-pages
-git push -f origin gh-pages:master
-git branch -D gh-pages
+REM git subtree split --prefix public -b gh-pages
+REM git push -f origin gh-pages:master
+REM git branch -D gh-pages
 git push -f
